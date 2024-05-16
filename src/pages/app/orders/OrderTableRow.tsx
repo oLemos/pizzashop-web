0578472198a1cfd8338670f3b4bb70e958836afe
oLemos <lemmos@outlook.com.br>
2024-05-16 10:16:32 -0300

    refactor(TableRow): isolate row component

---
+++ src/pages/app/orders/OrderTableRow.tsx
@@ -0,0 +1,48 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export const OrderTableRow = () => {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button size="xs" variant="outline">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Order details</span>
+        </Button>
+      </TableCell>
+
+      <TableCell className="font-mono text-xs font-medium">
+        423ds-fsd423fsd
+      </TableCell>
+
+      <TableCell className="text-muted-foreground">15 minutes ago</TableCell>
+
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pending</span>
+        </div>
+      </TableCell>
+
+      <TableCell className="font-medium">Gabriel Lemos Junqueira</TableCell>
+
+      <TableCell className="font-medium">$ 149,90</TableCell>
+
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          <span>Approve</span>
+        </Button>
+      </TableCell>
+
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          <span>Cancel</span>
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
