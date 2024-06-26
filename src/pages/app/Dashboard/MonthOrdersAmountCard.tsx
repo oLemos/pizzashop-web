5c22fb0cddba592e4e704ee2d796c8d9c72a67d9
oLemos <lemmos@outlook.com.br>
2024-06-26 20:35:04 -0300

    feat(Dashboard): month orders amount card integration

--- src/pages/app/Dashboard/MonthOrdersAmountCard.tsx
+++ src/pages/app/Dashboard/MonthOrdersAmountCard.tsx
@@ -1,8 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
 import { Utensils } from 'lucide-react'
 
+import { getMonthOrdersAmount } from '@/api/getMonthOrdersAmount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export const MonthOrdersAmountCard = () => {
+  const { data: monthOrdersAmount } = useQuery({
+    queryFn: getMonthOrdersAmount,
+    queryKey: ['metrics', 'month-orders-amount'],
+  })
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -13,12 +20,32 @@ export const MonthOrdersAmountCard = () => {
       </CardHeader>
 
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">$246</span>
+        {monthOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
 
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500">+6%</span> {''}
-          compared to last month
-        </p>
+            <p className="text-xs text-muted-foreground">
+              {monthOrdersAmount.diffFromLastMonth >= 0 ? (
+                <>
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    +{monthOrdersAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  {''}
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {monthOrdersAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  {''}
+                </>
+              )}
+              compared to last month
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
