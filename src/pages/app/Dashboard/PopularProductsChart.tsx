a32b295e1b110b799dda0a33c62766bd2b9c83b0
oLemos <lemmos@outlook.com.br>
2024-07-03 17:25:39 -0300

    feat(Dashboard): charts loading

--- src/pages/app/Dashboard/PopularProductsChart.tsx
+++ src/pages/app/Dashboard/PopularProductsChart.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { BarChart } from 'lucide-react'
+import { BarChart, Loader2 } from 'lucide-react'
 import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
@@ -34,7 +34,7 @@ export const PopularProductsChart = () => {
         </CardHeader>
 
         <CardContent>
-          {popularProducts && (
+          {popularProducts ? (
             <ResponsiveContainer width="100%" height={240}>
               <PieChart style={{ fontSize: 12 }}>
                 <Pie
@@ -90,6 +90,10 @@ export const PopularProductsChart = () => {
                 </Pie>
               </PieChart>
             </ResponsiveContainer>
+          ) : (
+            <div className="flex h-[240px] w-full items-center justify-center">
+              <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+            </div>
           )}
         </CardContent>
       </Card>
