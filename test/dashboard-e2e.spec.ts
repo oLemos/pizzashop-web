8a521b9b441697e99984b2af6adba4870c688277
oLemos <lemmos@outlook.com.br>
2024-07-29 07:31:25 -0300

    test: dashboard metrics display tests

---
+++ test/dashboard-e2e.spec.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test'
+
+test('Display day orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('20', { exact: true })).toBeVisible()
+  expect(page.getByText('-5% compared to yesterday')).toBeVisible()
+})
+
+test('Display month orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('200', { exact: true })).toBeVisible()
+  expect(page.getByText('+7% compared to last month')).toBeVisible()
+})
+
+test('Display total revenue metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('R$ 200,00')).toBeVisible()
+  expect(page.getByText('+10% compared to last month')).toBeVisible()
+})
+
+test('Display orders canceled metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('5', { exact: true })).toBeVisible()
+  expect(page.getByText('-5% compared to last month')).toBeVisible()
+})
